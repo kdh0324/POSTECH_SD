168308dd5f81da5eefe57db6a118c6c4286eacce
kdh0324 <kdh0324@postech.ac.kr>
2020-09-20 20:05:36 +0900

    Implement Tower classes

--- problem2/src/main/java/edu/postech/csed332/homework1/GroundTower.java
+++ problem2/src/main/java/edu/postech/csed332/homework1/GroundTower.java
@@ -1,7 +1,5 @@
 package edu.postech.csed332.homework1;
 
-import java.util.Set;
-
 /**
  * A ground tower that can attack nearby ground monsters within 1 tile of distance.
  * For example, a ground tower at (x,y) can attack any ground monsters at (x-1, y),
@@ -10,21 +8,10 @@ import java.util.Set;
  * Feel free to modify this file, e.g., adding new fields or methods. If needed,
  * you can define a new (abstract) super class that this class inherits.
  */
-public class GroundTower implements Tower {
+public class GroundTower extends TowerClass {
 
     public GroundTower(GameBoard board) {
-        // TODO: implement this
-    }
-
-    @Override
-    public Set<Monster> attack() {
-        // TODO: implement this
-        return null;
-    }
-
-    @Override
-    public GameBoard getBoard() {
-        // TODO: implement this
-        return null;
+        super(board);
+        isGround = true;
     }
 }
