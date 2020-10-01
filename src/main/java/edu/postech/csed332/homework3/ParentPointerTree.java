f89db61933c5da4a9477dfe0008868b6d306fd05
kdh0324 <kdh0324@postech.ac.kr>
2020-10-02 02:27:21 +0900

    Implement ParentPointerTree Class

--- src/main/java/edu/postech/csed332/homework3/ParentPointerTree.java
+++ src/main/java/edu/postech/csed332/homework3/ParentPointerTree.java
@@ -14,21 +14,6 @@ import java.util.stream.Collectors;
  */
 public class ParentPointerTree<N extends Comparable<N>> implements MutableTree<N> {
 
-    private static class Node<V> {
-        final @Nullable V parent;
-        final int depth;
-
-        Node(@Nullable V parent, int depth) {
-            this.parent = parent;
-            this.depth = depth;
-        }
-    }
-
-    /**
-     * A root vertex of this tree; {@code null} for an empty tree.
-     */
-    private @Nullable N root;
-
     /**
      * A map assigning to each vertex a pair of a parent reference and a depth. The parent
      * of the root is {@code null}. For example, a tree with four vertices {v1, v2, v3, v4}
@@ -36,6 +21,10 @@ public class ParentPointerTree<N extends Comparable<N>> implements MutableTree<N
      * the map {v1 |-> (null,0), v2 |-> (v1,1), v3 |-> (v1,1), v4 |-> (v2,2)}.
      */
     private final @NotNull SortedMap<N, Node<N>> nodeMap;
+    /**
+     * A root vertex of this tree; {@code null} for an empty tree.
+     */
+    private @Nullable N root;
 
     /**
      * Creates an empty parent pointer tree.
@@ -47,80 +36,90 @@ public class ParentPointerTree<N extends Comparable<N>> implements MutableTree<N
 
     @Override
     public @NotNull Optional<N> getRoot() {
-        // TODO: implement this
-        return Optional.empty();
+        if (root == null) return Optional.empty();
+        return Optional.of(root);
     }
 
     @Override
     public int getDepth(@NotNull N vertex) {
-        // TODO: implement this
-        return 0;
+        if (getRoot().isEmpty()) throw new IllegalStateException("Tree has no root.");
+        if (!containsVertex(vertex)) throw new IllegalArgumentException("The vertex in not in tree.");
+        return nodeMap.get(vertex).depth;
     }
 
     @Override
     public int getHeight() {
-        // TODO: implement this
-        return 0;
+        if (getRoot().isEmpty()) throw new IllegalStateException("Tree has no root.");
+        return nodeMap.get(nodeMap.lastKey()).depth;
     }
 
     @Override
     public boolean containsVertex(@NotNull N vertex) {
-        // TODO: implement this
-        return false;
+        return nodeMap.containsKey(vertex);
     }
 
     @Override
     public boolean addVertex(@NotNull N vertex) {
-        // TODO: implement this
-        return false;
+        if (getRoot().isPresent()) return false;
+        nodeMap.put(vertex, new Node<>(null, 0));
+        root = vertex;
+        return true;
     }
 
     @Override
     public boolean removeVertex(@NotNull N vertex) {
-        // TODO: implement this
-        return false;
+        if (!containsVertex(vertex)) return false;
+        Set<N> removed = new HashSet<>();
+        removed.add(vertex);
+        nodeMap.forEach((key, value) -> {
+            if (value.parent != null && removed.contains(value.parent))
+                removed.add(key);
+        });
+        removed.forEach(nodeMap::remove);
+        return true;
     }
 
     @Override
     public boolean containsEdge(@NotNull N source, @NotNull N target) {
-        // TODO: implement this
-        return false;
+        if (!containsVertex(target)) return false;
+        return Objects.equals(nodeMap.get(target).parent, source);
     }
 
     @Override
     public boolean addEdge(@NotNull N source, @NotNull N target) {
-        // TODO: implement this
-        return false;
+        if (!containsVertex(source) || containsVertex(target)) return false;
+        nodeMap.put(target, new Node<>(source, getHeight()));
+        return true;
     }
 
     @Override
     public boolean removeEdge(@NotNull N source, @NotNull N target) {
-        // TODO: implement this
-        return false;
+        if (!containsVertex(source)) return false;
+        return removeVertex(target);
     }
 
     @Override
     public @NotNull Set<N> getSources(N target) {
-        // TODO: implement this
-        return Collections.emptySet();
+        return Collections.singleton(nodeMap.get(target).parent);
     }
 
     @Override
     public @NotNull Set<N> getTargets(N source) {
-        // TODO: implement this
-        return Collections.emptySet();
+        return nodeMap.keySet().stream()
+                .filter(target -> Objects.equals(nodeMap.get(target).parent, source))
+                .collect(Collectors.toUnmodifiableSet());
     }
 
     @Override
     public @NotNull Set<N> getVertices() {
-        // TODO: implement this
-        return Collections.emptySet();
+        return nodeMap.keySet();
     }
 
     @Override
     public @NotNull Set<Edge<N>> getEdges() {
-        // TODO: implement this
-        return Collections.emptySet();
+        return nodeMap.entrySet().stream()
+                .map(entry -> new Edge<>(entry.getKey(), entry.getValue().parent))
+                .collect(Collectors.toUnmodifiableSet());
     }
 
     /**
@@ -129,8 +128,12 @@ public class ParentPointerTree<N extends Comparable<N>> implements MutableTree<N
      * @return true if the representation of this tree is valid
      */
     boolean checkInv() {
-        // TODO: implement this
-        return false;
+        return nodeMap.values().stream()
+                .allMatch(node -> {
+                    if (node.parent == null)
+                        return true;
+                    return containsVertex(node.parent);
+                });
     }
 
     /**
@@ -144,5 +147,15 @@ public class ParentPointerTree<N extends Comparable<N>> implements MutableTree<N
                 root,
                 getVertices().stream().map(N::toString).collect(Collectors.joining(", ")),
                 getEdges().stream().map(Edge::toString).collect(Collectors.joining(", ")));
+    }
+
+    private static class Node<V> {
+        final @Nullable V parent;
+        final int depth;
+
+        Node(@Nullable V parent, int depth) {
+            this.parent = parent;
+            this.depth = depth;
+        }
     }
 }
