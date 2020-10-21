9f420a5fbd9f26e0e3ed578cdf2686457553ebca
kdh0324 <kdh0324@postech.ac.kr>
2020-10-22 03:13:56 +0900

    Implement ToStringVisitor

--- src/main/java/edu/postech/csed332/homework5/ToStringVisitor.java
+++ src/main/java/edu/postech/csed332/homework5/ToStringVisitor.java
@@ -1,11 +1,29 @@
 package edu.postech.csed332.homework5;
 
+import edu.postech.csed332.homework5.expression.BinaryExp;
+import edu.postech.csed332.homework5.expression.NumberExp;
+import edu.postech.csed332.homework5.expression.VariableExp;
+
 /**
  * A visitor to compute the string expression of a given expression
  */
 public class ToStringVisitor implements ExpVisitor<String> {
+    @Override
+    public String visitBinary(BinaryExp binaryExp, String operator) {
+        String left = binaryExp.getLeft().toString();
+        String right = binaryExp.getRight().toString();
+        return "(" + left + operator + right + ")";
+    }
 
-    // TODO write and implement the visitor methods for ToStringVisitor, satisfying
-    //  the specification of Exp.toString. A double value is written using exponents,
-    //  e.g., using Double.toString().
+    @Override
+    public String visitNumber(NumberExp numberExp) {
+        String value = numberExp.getValue().toString();
+        if (numberExp.getValue() < 0) return "(" + value + ")";
+        return value;
+    }
+
+    @Override
+    public String visitVariable(VariableExp variableExp) {
+        return "x" + variableExp.getName();
+    }
 }
